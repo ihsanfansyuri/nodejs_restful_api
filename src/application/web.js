ea4ba31155a7c736ea698f77b632c3828dd0d387
ihsanfansyuri <67683530+ihsanfansyuri@users.noreply.github.com>
2023-08-22 02:09:11 +0800

    add handle cors

--- src/application/web.js
+++ src/application/web.js
@@ -2,8 +2,24 @@ import express from "express";
 import { publicRouter } from "../route/public-api.js";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { userRouter } from "../route/api.js";
+import cors from "cors";
 
 export const web = express();
+web.use(
+  cors({
+    origin: [process.env.WEB_URL, process.env.SERVER_URL],
+    credentials: true, //access-control-allow-credentials:true
+    allowedHeaders: [
+      "Access-Control-Allow-Headers",
+      "Origin, Accept",
+      "X-Requested-With",
+      "Content-Type",
+      "Access-Control-Request-Method",
+      "Access-Control-Request-Headers",
+    ],
+    optionSuccessStatus: 200,
+  })
+);
 web.use(express.json());
 
 web.use(publicRouter);
